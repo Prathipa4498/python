8a3c4bf452ed780f62772505ff09ccf03ce2f84b
Prathipa <46566615+Prathipa4498@users.noreply.github.com>
2019-01-19 17:15:55 +0530

    Create arms.py

---
+++ arms.py
@@ -0,0 +1,13 @@
+lw=int(input())
+up=int(input())
+for num in range(lw,up + 1):
+   order = len(str(num))
+   sum = 0
+   temp = num
+   while temp > 0:
+       digit = temp % 10
+       sum += digit ** order
+       temp //= 10
+ 
+   if num == sum:
+       print(num)
