8cc11c5df73bf5a70ec4353bcf5fecd57b41e5e1
Prathipa <46566615+Prathipa4498@users.noreply.github.com>
2019-06-05 00:19:55 -0700

    Create anagrm.py

---
+++ anagrm.py
@@ -0,0 +1,13 @@
+from itertools import permutations
+values=list(permutations(['d','h','o','n','i']))
+x=input()
+m=[]
+for i in values:
+  if i not in m:
+    m.append(''.join(i))
+for i in m:
+  if i==x:
+    print('yes')
+    break
+else:
+  print('no')
