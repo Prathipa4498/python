3bbdb613a56a3d6e2d314d27be923f17ee90abac
Prathipa <46566615+Prathipa4498@users.noreply.github.com>
2019-06-05 07:07:06 -0700

    Update pra33.py

--- pra33.py
+++ pra33.py
@@ -1,12 +1,12 @@
-str=input()
-flag=0
-for i in range(0,len(str)-1):
-  for j in range(i+1,len(str)):
-    if str[i]<str[k]:
-      flag=1
-      print(str[k:])
+st=input()
+flag3=0
+for i in range(0,len(st)-1):
+  for j in range(i+1,len(st)):
+    if st[i]<st[j]:
+      flag3=1
+      print(st[j:])
       break
-  if flag==1:
+  if flag3==1:
     break
 else:
-  print(str)
+  print(st)
