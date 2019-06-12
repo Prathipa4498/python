2268012ed5c59e4abc9c092fd851cceb32264639
Prathipa <46566615+Prathipa4498@users.noreply.github.com>
2019-06-12 06:01:42 -0700

    Create pro55.py

---
+++ pro55.py
@@ -0,0 +1,13 @@
+num,k=map(int,input().split())
+l=[str(i) for i in input().split()]
+q=[]
+m=list(combinations_with_replacement(l,k))
+for i in range(0,len(m)):
+    t = list(m[i])
+    s=0
+    for j in range(0,k):
+       s=s+int(t[j])
+    q.append(s)
+q=list(set(q))
+q=sorted(q)
+for i in range(0,len(q)): print(q[i],end=" ")
