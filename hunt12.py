78a619e2c46647cc50224007166d5f75b20aaab3
Prathipa <46566615+Prathipa4498@users.noreply.github.com>
2019-06-12 00:16:34 -0700

    Create hunt12.py

---
+++ hunt12.py
@@ -0,0 +1,10 @@
+S,T=map(int,raw_input().split())
+a=list(map(int,raw_input().split()))
+sum=0
+for i in range(0,T):
+  u,v=map(int,raw_input().split())
+  b=a[u-1:v]
+  for j in b:
+    sum=sum+j
+  print(sum)
+  sum=0
