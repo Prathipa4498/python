802f48a922b45d2625a047086e77d04c6719d456
Prathipa <46566615+Prathipa4498@users.noreply.github.com>
2019-06-12 01:25:16 -0700

    Create pro57.py

---
+++ pro57.py
@@ -0,0 +1,14 @@
+import syste,string, math, itertools
+
+a1, a2 = input().split()
+n = len(a1)
+for j in range(1,0,-1) :
+    #print('arr len = ', j+1)
+    for i in range(0,n-j) :
+        li, ri = i,j+i
+        a3 = a1[li:ri + 1]
+        #print(li, ri, a3)
+        if a3 in a2 :
+            print('yes')
+            syste.exit()
+print('no')
