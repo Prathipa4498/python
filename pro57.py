890ad901a179e6128f3e29186a2cb4e7da4d6171
Prathipa <46566615+Prathipa4498@users.noreply.github.com>
2019-06-12 01:28:07 -0700

    Update pro57.py

--- pro57.py
+++ pro57.py
@@ -1,14 +1,11 @@
-import syste,string, math, itertools
-
-a1, a2 = input().split()
-n = len(a1)
-for j in range(1,0,-1) :
-    #print('arr len = ', j+1)
-    for i in range(0,n-j) :
-        li, ri = i,j+i
-        a3 = a1[li:ri + 1]
-        #print(li, ri, a3)
-        if a3 in a2 :
-            print('yes')
-            syste.exit()
-print('no')
+s,t=map(str,input().split())
+c=0
+for i in range(0,len(s)):
+    for j in range(0,len(t)):
+        if s[i]==t[j]:
+            c=c+1
+            #print(s[i],"==",t[j])
+if c>=2:
+    print("yes")
+else:
+    print("no")
