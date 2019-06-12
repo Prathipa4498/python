ac9f16f54cef55860932df66fb0e9cbdc350bf1a
Prathipa <46566615+Prathipa4498@users.noreply.github.com>
2019-06-12 00:28:42 -0700

    Update hunter31.py

--- hunter31.py
+++ hunter31.py
@@ -1,12 +1,12 @@
-n1=int(input())
+n=int(input())
 l=[int(i) for i in input().split()]
 a=[]
-for i in range(0,n1):
+for i in range(0,n):
     c=1
-    for j in range(i,n1):
+    for j in range(i,n):
         c=c*l[j]
     a.append(c)
-for i in range(0,n1):
+for i in range(0,n):
     c=1
     for j in range(i+1):
         c=c*l[j]
