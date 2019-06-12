0292097bef326b683f258692922417d07a073eb4
Prathipa <46566615+Prathipa4498@users.noreply.github.com>
2019-06-12 00:19:59 -0700

    Update hunt12.py

--- hunt12.py
+++ hunt12.py
@@ -1,7 +1,7 @@
-S,T=map(int,raw_input().split())
+N,Q=map(int,raw_input().split())
 a=list(map(int,raw_input().split()))
 sum=0
-for i in range(0,T):
+for i in range(0,Q):
   u,v=map(int,raw_input().split())
   b=a[u-1:v]
   for j in b:
