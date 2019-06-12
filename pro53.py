53f910fceeae49cda773e8d5c5b93d76df62c2c0
Prathipa <46566615+Prathipa4498@users.noreply.github.com>
2019-06-12 01:20:50 -0700

    Update pro53.py

--- pro53.py
+++ pro53.py
@@ -1,11 +1,8 @@
-b=str(input())
-b=b.lower()
-l=len(b)
-L=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-for i in range(0,l):
-	if(b[i] in L):
-		L.remove(b[i])
-if(len(L)==0):
-	print("yes")
+t=raw_input()
+n=t.lower()
+h=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+if all(i in n for i in h):
+    print("yes")
 else:
-	print("no")
+    print("no")
